95ddb22be66d9066d1db8a009d3add394d349ece
Chase <chasewilliams98@gmail.com>
2023-05-01 00:39:38 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoService from './service/mongo-service';
 
 dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-	mongoService.getCollection('users').then((collection) => {
-		res.send(collection);
-	});
-	// res.status(500).send('Something broke!');
-});
+app.use('/users', require('./routes/users'));
 
 app.listen(3000, () => {
 	console.log('Server is running on port 3000');
